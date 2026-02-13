ccddcdd19a4fc2117de28d46ab2b748cb7860f61
smitb7 <smitbhavsar7@icloud.com>
2026-02-13 14:52:36 +0530

    Day-5

---
+++ Day-5/Javascript/script.js
@@ -0,0 +1,7 @@
+document.getElementById("demo").innerHTML= "Hellow World";  //statement 
+
+console.log("Hello");
+
+function myFunction(params) {
+    document.getElementById("demo2").innerHTML = "text changed"
+}
