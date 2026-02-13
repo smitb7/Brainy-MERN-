a96ddefe12579d7a0e40003c82b44c3283dde042
smitb7 <smitbhavsar7@icloud.com>
2026-02-13 20:16:50 +0530

    day5

--- Day-5/Javascript/script.js
+++ Day-5/Javascript/script.js
@@ -1,7 +1,12 @@
-document.getElementById("demo").innerHTML= "Hellow World";  //statement 
+// document.getElementById("demo").innerHTML= "Hellow World";  //statement 
 
-console.log("Hello");
+// console.log("Hello");
 
-function myFunction(params) {
-    document.getElementById("demo2").innerHTML = "text changed"
-}
+// function myFunction(params) {
+//     document.getElementById("demo2").innerHTML = "text changed"
+// }
+
+let num = 123;
+let str = toString(num) ;
+
+console.log(typeof str);
