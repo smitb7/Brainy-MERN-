6dfe00dad56fd6fc5420cb7d40ea1636932dbd24
smitb7 <smitbhavsar7@icloud.com>
2026-02-15 11:12:52 +0530

    Lecture tasks JS

---
+++ Day-5/lecture_tasks/even_odd.js
@@ -0,0 +1,12 @@
+
+let number = 54
+
+if(number%2==0){
+    console.log("It's Even Number");
+}
+else{
+    console.log("It's Odd Number");
+    
+}
+
+
---
+++ Day-5/lecture_tasks/greaternum.js
@@ -0,0 +1,12 @@
+// write a program that checks if a number is greater than 10 . if it is , print  " > 10" otherwise "10 or less"
+
+
+let number = 10
+
+if(number > 10){
+    console.log("Greater than 10")
+
+}
+else{
+    console.log("10 or less")
+}
