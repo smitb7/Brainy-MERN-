16721ec01cab03549c59af5494a483007db297de
smitb7 <smitbhavsar7@icloud.com>
2026-02-15 21:09:11 +0530

    Lecture tasks JS

---
+++ Day-5/lecture_tasks/storediscount.js
@@ -0,0 +1,16 @@
+
+let totalpurchaseammount = $120;
+let discount = totalpurchaseammount * 0.10 ;
+let afterdiscountpayable = totalpurchaseammount - discount ;
+
+if(totalpurchaseammount > $100){
+    
+    console.log("You are applicable for 10% Discount and you have to pay, you have to pay" + );
+    
+
+}
+else{
+    console.log("You aren't eligible for discount but we will give you 5 toffieess..🤓 ╾━╤デ╦︻ (•_- )!!");
+    
+}
+
