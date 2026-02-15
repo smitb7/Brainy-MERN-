7d23817f1ee60987ce9feb95f2265eeb9f160a04
smitb7 <smitbhavsar7@icloud.com>
2026-02-15 14:38:58 +0530

    Lecture tasks JS

---
+++ Day-5/lecture_tasks/vowel_consonant.js
@@ -0,0 +1,15 @@
+// vowel and consonants
+// we are checking for both small and capital
+
+let char = 'b' ;
+
+if(    char == 'A' || char == 'a'  
+    || char == 'E' || char == 'e' 
+    || char == 'I' || char == 'i' 
+    || char == 'O' || char == 'o' 
+    || char == 'U' || char == 'u') {
+    console.log("This alphabet is Vowel");
+}else{
+    console.log("this alphabet is Consonant");
+    
+}
