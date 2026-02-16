f81cab6a583d56848c117c786749629a1a08b818
smitb7 <smitbhavsar7@icloud.com>
2026-02-16 23:29:32 +0530

    Day-6

---
+++ Day-6/constructor.js
@@ -0,0 +1,17 @@
+//  constructor
+
+function Student(first, last , age , color ) {
+
+    this.firstname = first,
+    this.lastname = last,
+    this.age = age ,
+    this.color = color
+    
+}
+
+const Studentram = new Student("Ram ", "Raghav", "4000", "Medium")
+
+console.log(Studentram);
+
+
+// so that we can use this again and again for adding the students,  like this is a blue print of the adding the students. 
--- Day-6/object.js
+++ Day-6/object.js
@@ -23,4 +23,18 @@ for (let i in person1){
 
 // bject.values (obj convert in arr )
 
-const arr = Object.values(person1)
+// const arr = Object.values(person1)
+
+
+
+// entries (if we want to print key and values both )
+
+let text1 = "";
+
+for (let [key, value] of Object.entries(person1)) {
+
+    text1 += key + " : " + value + "<br>";
+}
+
+console.log(text1);
+
