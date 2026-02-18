f11c34a9f6d28251d73fe0b84c3043c7cd584bd3
smitb7 <smitbhavsar7@icloud.com>
2026-02-18 23:30:59 +0530

    React-Components

--- Day-8/react_components/my-app/src/App.js
+++ Day-8/react_components/my-app/src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hellowww from react...!</h1>
+      <Button btn="Download"/>
+      <br />
+      <Button btn="Upload" />
+      <br />
+      <Button btn="Click Me" />
+    </>
   );
 }
 
---
+++ Day-8/react_components/my-app/src/button.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+const Button = (props) => {
+  return (
+    <>
+      <button>{props.btn }</button>
+
+      
+
+
+
+    </>
+  );
+};
+
+export default Button;
