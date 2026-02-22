8f6b6719f5b1d9a790c1f85651e66b13e566dde7
smitb7 <smitbhavsar7@icloud.com>
2026-02-22 17:58:50 +0530

    Hooks

--- Day-10/dynamic-routs-hooks/Dynamic-Routs-Demo/src/pages/Home.jsx
+++ Day-10/dynamic-routs-hooks/Dynamic-Routs-Demo/src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
 const Home = () => {
+
+    const myStyle ={
+
+      color: "white",
+      backgroundColor: "black",
+      padding: "10px"
+    }
+
+
   return (
     <div>
-      <h2 >
+      <h2 style={myStyle} >
         This is Your Home Page
       </h2>
     </div>
